f6395bd7bf099702e7bbda7f8a0c161d8e9185b3
eric felts <echristianfelts@gmail.com>
2020-01-22 17:29:54 -0800

    Player movement bounds and horizontal wrap

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,6 +24,33 @@ public class Player : MonoBehaviour
         Vector3 playerVector = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(playerVector * Time.deltaTime * _playerSpeed); 
 
+        //if player y > than 0
+        //then y = 0
+        //if player x < -11
+        //then x = 11
+        //if x > 11
+        //then x = -11
+
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+
+        } else if (transform.position.y <-4)
+        {
+            transform.position = new Vector3(transform.position.x, -4, 0);
+        }
+
+        if (transform.position.x > 10.5)
+        {
+            transform.position = new Vector3(-10, transform.position.y, 0);
+        }
+        else if (transform.position.x < -10.5)
+        {
+            transform.position = new Vector3(10, transform.position.y, 0);
+        }
+
+
+
 
     }
 }
