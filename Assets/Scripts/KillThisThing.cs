c429f83084c195469126e194d797471c700964cd
eric felts <echristianfelts@gmail.com>
2020-01-24 14:02:07 -0800

    added kill me script to tripleshot prefab.

--- Assets/Scripts/KillThisThing.cs
+++ Assets/Scripts/KillThisThing.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class KillThisThing : MonoBehaviour
 {
+    private float lifetime = 3.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(IEKillThisThing(lifetime));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator IEKillThisThing(float killtime)
+    {
+        yield return new WaitForSeconds(killtime);
+        Destroy(this.gameObject);
     }
 }
