b421f822032f0bea87571cfe8caf75bb98566e17
eric felts <echristianfelts@gmail.com>
2020-01-24 14:47:27 -0800

    Triple Shot Powerup Behaviour impliment.

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move down at 3 units
+        transform.Translate(Vector3.down * Time.deltaTime * _speed);
+        // When we leave the screen... 
+        if (transform.position.y < -6f)
+        {
+            // Destroy 
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    // onTriggerEnter
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // only collectable by player. (use Tags)
+        // when collected...  Destroy.
+        if (other.tag == "Player")
+        {
+
+            //Player playerTest = other.transform.GetComponent<Player>();
+
+            Debug.Log("TRIGGER: Power Up Collected");
+
+            Destroy(this.gameObject);
+
+        }
+
+
+    }
+
+}
