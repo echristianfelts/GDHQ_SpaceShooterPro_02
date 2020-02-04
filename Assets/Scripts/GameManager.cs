70be0421e05b29a8853d3738de51736b68a6247f
eric felts <echristianfelts@gmail.com>
2020-02-03 16:20:28 -0800

    R Key to Restart Level

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private bool _isGameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if the r key was pressed...
+        if (Input.GetKeyDown(KeyCode.R) && _isGameOver == true)
+        {
+            SceneManager.LoadScene(0);  //Current Game Scene 
+        }
+        //restartt the current scene.
+
+    }
+
+
+    public void GameOver()
+    {  
+    _isGameOver = true;
+    }
+
+}
