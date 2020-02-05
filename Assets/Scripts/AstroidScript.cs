3087be2164595554c0d8b961a3f734877ff25f71
eric felts <echristianfelts@gmail.com>
2020-02-04 20:12:03 -0800

    Asteroid Behaviour 'Rotation'

---
+++ Assets/Scripts/AstroidScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroidScript : MonoBehaviour
+{
+    private float _rotSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //constatnly rotate astroid on z
+        //_rotSpeed += 1;
+        transform.Rotate(Vector3.forward * _rotSpeed);
+    }
+}
